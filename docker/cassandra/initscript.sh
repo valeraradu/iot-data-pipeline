c1c35f495fe05047be522458808f5d72ba32b079
valeraradu@hotmail.com <valeraradu@hotmail.com>
2018-04-10 22:37:43 +0300

    dockers ready

--- docker/cassandra/initscript.sh
+++ docker/cassandra/initscript.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cassandra -R &
-
 for f in /init_table/*; do
   echo "$f"
   until cqlsh --username cassandra --password cassandra -f "$f"; do
    echo "Cassandra is unavailable - sleeping"; sleep 10;
   done
-done
+done &
 
-exec "$@"
+exec /app-entrypoint.sh "$@"
